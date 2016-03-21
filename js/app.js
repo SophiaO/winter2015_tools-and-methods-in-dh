eff6228fb8709a1b23e2668a4e2ac8cd86762503
Sophia O <sophia_oppermann@web.de>
2016-03-21 14:19:17 +0100

    Eins

---
+++ js/app.js
@@ -0,0 +1,28 @@
+
+	
+
+
+$(document).ready (function() {
+	
+	
+	$('.overlay').hide();
+	$('.overlay').slideDown(5000);
+	
+	
+	$('.not').hide();
+	
+	$('.teaserbildEnde').mouseover (function() {
+	$('.not').slideDown(5000);
+	});
+	
+	
+	
+});
+		
+	
+	
+	
+
+	
+	
+						
